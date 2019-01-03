c314d32a13472ddbba120b76573e3c4743bdf5dc
Dan <qzy555@yahoo.com>
2019-01-03 16:37:31 -0500

    simple interface added

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# dripdrip
+# DripDrop
+
+BitTorrent client as python learning project.
+
+DripDrop is a BitTorrent client that you shouldn't use. Currently it downloads files, but does not yet upload them.
--- dripdrop.py
+++ dripdrop.py
@@ -1,10 +1,8 @@
+import os
+from client import Client
+from torrent import TorrentError
+
 # TODO: Handle problem if tracker is unavailable.
-
-# TODO: Allow Queue threads to exhaust themselves.
-
-# TODO:
-#     1. When we receive a handshake, we should send an interested signal.
-#     2. Once we are unchoked, we should request a piece from the peer.
 
 # TODO: Store Partial File Download? Perhaps just completed pieces - But
 #       allow for reinitiailzation on Torrent load
@@ -19,6 +17,31 @@
 # 2. Torrent
 # 3. Piece
 # 4. Message
+# 5. Peer
+
+client = Client()
+
+print(" __              __")
+print("|  \\  __    __  |  \\  __  __   __")
+print("|__/ |   | |__| |__/ |   |__| |__|")
+print("           |                  |")
+
+def get_and_assign_file():
+    file_name = input(".torrent file: ")
+
+    if os.path.splitext(file_name)[-1] != '.torrent':
+        print("Must be .torrent file")
+        get_and_assign_file()
+    try:
+        client.add_torrent(file_name)
+    except TorrentError as e:
+        print(e)
+        get_and_assign_file()
+
+get_and_assign_file()
+client.start_torrent()
+
+
 
 
 
